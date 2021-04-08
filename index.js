0be9619238edb03bd85a95bdcb612aca7ae57da6
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-04-08 09:57:32 -0700

    wip

---
+++ index.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const server = express()
+const port = process.env.PORT || 9000
+
+server.use(cors())
+server.use(express.json())
+
+server.use("/api/", (_, res) => {
+    res.json({ data: "API is accounted for" })
+})
+
+server.listen(port, () => {
+    console.log(`Server is alive on port ${port}`)
+})
+
+// console.log("Its alive!")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USERNAME)
+// console.log(process.env.PORT)
+// console.log(process.env.FOOD)
+// console.log(process.env.REALLY)
+// console.log(process.env.GREET)
